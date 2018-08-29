6859c5a231b4a33335cdad89b6f73401fd9eba5c
yv4321 <yv4321@accept.io>
2018-08-29 20:09:53 +0300

    Added openzeppeling contracts and chai.

--- migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-var Token = artifacts.require("./FulcrumToken.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Token, "0xd028820281561e97fdf811ca777c3270c92da464", 5000000000000, 60);
-};
