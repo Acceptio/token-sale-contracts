708d8dcd0a5f0f8ee08c8a7a8bb0fec8e68cd77f
Binod Nirvan <binodnp@outlook.com>
2018-09-01 00:27:51 +0545

    Refactoring
    - Refactored data types.
    - Updated the constant name "TOKEN_TOTAL_ALLOCATION" to "MAX_SUPPLY" for brevity.
    - Dropped redundant ERC20 functions already implemented in Open Zeppelin.

--- contracts/FulcrumToken.sol
+++ contracts/FulcrumToken.sol
@@ -16,15 +16,15 @@ contract FulcrumToken is StandardToken, BurnableToken, Claimable, HasNoContracts
     uint256 public tokenPrice;
     uint256 public tokenPriceDiscount;
 
-    uint public startTimestamp;
-    uint public icoDuration;
+    uint256 public startTimestamp;
+    uint256 public icoDuration;
 
     uint256 public discountRate = 0;
 
     uint256 public totalRaised = 0;
     address public fundsWallet;
     
-    uint256 public constant TOKEN_TOTAL_ALLOCATION = 400 * (10**6) * (10**decimals);  
+    uint256 public constant MAX_SUPPLY = 400 * (10**6) * (10**decimals);  
     uint256 public constant HARD_CAP = 150 * (10**6) * (10**decimals);
     uint256 public constant SOFT_CAP = 1 * (10**6) * (10**decimals);
 
@@ -34,8 +34,11 @@ contract FulcrumToken is StandardToken, BurnableToken, Claimable, HasNoContracts
     mapping (address => mapping (address => uint256)) private allowed;
     
     constructor(address _fundsWallet, uint256 _usdPrice, uint _icoDuration) public {
+        
         balances[address(this)] = HARD_CAP;
-        balances[_fundsWallet] = TOKEN_TOTAL_ALLOCATION.sub(HARD_CAP);
+        balances[_fundsWallet] = MAX_SUPPLY.sub(HARD_CAP);
+        totalSupply_ = MAX_SUPPLY;
+
         fundsWallet = _fundsWallet;
         tokenPrice = _usdPrice;
         startTimestamp = now;
@@ -115,141 +118,5 @@ contract FulcrumToken is StandardToken, BurnableToken, Claimable, HasNoContracts
     function setTokenPrice(uint256 _tokenPrice) onlyOwner public {
         tokenPrice = _tokenPrice;
         discountRate = 30; //for discount recalculate
-    }
-
-    /**
-    * @dev Total number of tokens in existence
-    */
-    function totalSupply() public view returns (uint256) {
-        return TOKEN_TOTAL_ALLOCATION;
-    }
-
-    /**
-    * @dev Gets the balance of the specified address.
-    * @param _owner The address to query the the balance of.
-    * @return An uint256 representing the amount owned by the passed address.
-    */
-    function balanceOf(address _owner) public view returns (uint256) {
-        return balances[_owner];
-    }
-
-    /**
-    * @dev Function to check the amount of tokens that an owner allowed to a spender.
-    * @param _owner address The address which owns the funds.
-    * @param _spender address The address which will spend the funds.
-    * @return A uint256 specifying the amount of tokens still available for the spender.
-    */
-    function allowance(
-        address _owner,
-        address _spender
-    )
-        public
-        view
-        returns (uint256)
-    {
-        return allowed[_owner][_spender];
-    }
-
-    /**
-    * @dev Transfer token for a specified address
-    * @param _to The address to transfer to.
-    * @param _value The amount to be transferred.
-    */
-    function transfer(address _to, uint256 _value) public returns (bool) {
-        require(_value <= balances[address(this)]);
-        require(_to != address(0));
-
-        balances[address(this)] = balances[address(this)].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        emit Transfer(address(this), _to, _value);
-        return true;
-    }
-
-    /**
-    * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
-    * Beware that changing an allowance with this method brings the risk that someone may use both the old
-    * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
-    * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
-    * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-    * @param _spender The address which will spend the funds.
-    * @param _value The amount of tokens to be spent.
-    */
-    function approve(address _spender, uint256 _value) public returns (bool) {
-        allowed[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    /**
-    * @dev Transfer tokens from one address to another
-    * @param _from address The address which you want to send tokens from
-    * @param _to address The address which you want to transfer to
-    * @param _value uint256 the amount of tokens to be transferred
-    */
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _value
-    )
-        public
-        returns (bool)
-    {
-        require(_value <= balances[_from]);
-        require(_value <= allowed[_from][msg.sender]);
-        require(_to != address(0));
-
-        balances[_from] = balances[_from].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-        emit Transfer(_from, _to, _value);
-        return true;
-    }
-
-    /**
-    * @dev Increase the amount of tokens that an owner allowed to a spender.
-    * approve should be called when allowed[_spender] == 0. To increment
-    * allowed value is better to use this function to avoid 2 calls (and wait until
-    * the first transaction is mined)
-    * From MonolithDAO Token.sol
-    * @param _spender The address which will spend the funds.
-    * @param _addedValue The amount of tokens to increase the allowance by.
-    */
-    function increaseApproval(
-        address _spender,
-        uint256 _addedValue
-    )
-        public
-        returns (bool)
-    {
-        allowed[msg.sender][_spender] = (
-        allowed[msg.sender][_spender].add(_addedValue));
-        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-        return true;
-    }
-
-    /**
-    * @dev Decrease the amount of tokens that an owner allowed to a spender.
-    * approve should be called when allowed[_spender] == 0. To decrement
-    * allowed value is better to use this function to avoid 2 calls (and wait until
-    * the first transaction is mined)
-    * From MonolithDAO Token.sol
-    * @param _spender The address which will spend the funds.
-    * @param _subtractedValue The amount of tokens to decrease the allowance by.
-    */
-    function decreaseApproval(
-        address _spender,
-        uint256 _subtractedValue
-    )
-        public
-        returns (bool)
-    {
-        uint256 oldValue = allowed[msg.sender][_spender];
-        if (_subtractedValue >= oldValue) {
-            allowed[msg.sender][_spender] = 0;
-        } else {
-            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
-        }
-        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-        return true;
     }
 }
